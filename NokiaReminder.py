63879604ceab78f62baedb2c3d6b2a404d7b90ac
rafael <rafaeldm1@al.insper.edu.br>
2021-11-12 00:06:46 -0300

    Vídeo 2 feito - desenho de figuras

--- NokiaReminder.py
+++ NokiaReminder.py
@@ -16,4 +16,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+    pygame.draw.rect(tela, (255,0,0), (300,400,50,60)) 
+    pygame.draw.circle(tela, (0,180,0), (350,290), 45)
+    pygame.draw.line(tela, (255,215,0), (370,0), (370,600), 6)
+
     pygame.display.update()
