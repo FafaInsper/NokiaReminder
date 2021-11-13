1d791cb60f522d191ed5780ed753b5040ca309a6
Fabricio <fabriciocl@al.insper.edu.br>
2021-11-13 19:28:02 -0300

    Movimentação

--- NokiaReminder.py
+++ NokiaReminder.py
@@ -1,23 +1,33 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-
+ 
 pygame.init() #Iniciando funções do pygame
-
+ 
 larguratela = 700
 alturatela = 700
 nome = "Nokia Reminder"
-
+x = larguratela/2
+y = 0
+ 
 tela = pygame.display.set_mode((larguratela, alturatela))
 pygame.display.set_caption(nome)
-
+ 
+clock = pygame.time.Clock()
+ 
 while True:
+    clock.tick(280)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255,0,0), (300,400,50,60)) 
-    pygame.draw.circle(tela, (0,180,0), (350,290), 45)
-    pygame.draw.line(tela, (255,215,0), (370,0), (370,600), 6)
-
-    pygame.display.update()
+ 
+    pygame.draw.rect(tela, (255,0,0), (x,y,50,60))
+ 
+    if y >= alturatela:
+        y = 0
+ 
+    y = y + 1
+ 
+    pygame.display.update
