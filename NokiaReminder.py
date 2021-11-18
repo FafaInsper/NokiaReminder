aed34e7c8025d4e60cea18503ba11ef096825b4f
JoaoPedroMSilva <joaopsm1@al.insper.edu.br>
2021-11-18 10:10:57 -0300

    coisao

--- NokiaReminder.py
+++ NokiaReminder.py
@@ -1,20 +1,24 @@
 import pygame
 from pygame.locals import *
 from sys import exit
- 
+from random import randint
+
 pygame.init() #Iniciando funções do pygame
- 
+
 larguratela = 700
 alturatela = 700
 nome = "Nokia Reminder"
 x = larguratela/2
 y = alturatela/2
- 
+
+x_azul = randint(50, 650)
+y_azul = randint(60, 640)
+
 tela = pygame.display.set_mode((larguratela, alturatela))
 pygame.display.set_caption(nome)
- 
+
 clock = pygame.time.Clock()
- 
+
 while True:
     clock.tick(280)
     tela.fill((0, 0, 0))
@@ -22,7 +26,6 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-        ...
 
         if event.type == KEYDOWN:
             if event.key == K_a:
@@ -42,12 +45,12 @@ while True:
             y = y - 35
         if pygame.key.get_pressed()[K_s]:
             y = y + 35 
-   
-   
-    pygame.draw.rect(tela, (255,0,0), (x,y,50,60))
- 
-    
- 
-    pygame.display.update
 
-    
+    ret_vermleho = pygame.draw.rect(tela, (255,0,0), (x,y,50,60)) 
+    ret_azul = pygame.draw.rect(tela, (0, 0, 255), (x_azul, y_azul, 50, 60))
+   
+    if ret_vermleho.colliderect(ret_azul):
+        x_azul = randint(50, 650)
+        y_azul = randint(60, 640)
+
+    pygame.display.update()
