a070e96958403890b461310b0364dc2e3491983a
Fabricio <fabriciocl@al.insper.edu.br>
2021-11-24 15:58:58 -0300

    Mortes + Velocidade

--- NokiaReminder.py
+++ NokiaReminder.py
@@ -35,11 +35,14 @@ clock = pygame.time.Clock()
 
 lista_cobra = []
 comprimento_inicial = 5
+morte = False
+
 
 def aumento_cobra(lista_cobra):
     for xey in lista_cobra:
         #xey = [x, y]
         pygame.draw.rect(tela, (0, 255, 0), (xey[0], xey[1], 20, 20))
+
 
 while True:
     clock.tick(280)
@@ -88,6 +91,7 @@ while True:
     if cobra.colliderect(maca):
         x_maca = randint(50, 650)
         y_maca = randint(60, 640)
+        velocidade = velocidade + 0.05
         pontos = pontos + 1
         barulho_colisao.play()
         comprimento_inicial = comprimento_inicial + 10
@@ -96,6 +100,33 @@ while True:
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
     lista_cobra.append(lista_cabeca)    
+
+    if lista_cobra.count(lista_cabeca) > 1:
+        morte = True
+        
+    if x_cobra > larguratela:
+        morte = True
+    if x_cobra < 0:
+        morte = True
+    if y_cobra > larguratela:
+        morte = True
+    if y_cobra < 0:
+        morte = True
+
+    
+    while morte:
+        mensagem_morte = f'Game Over! Você fez {pontos} pontos!'
+        fontemorte = pygame.font.SysFont('comic sans', 20, True, False)
+        textofinalmorte = fontemorte.render(mensagem_morte, True, (0,0,0))
+        ret_texto = textofinalmorte.get_rect()
+        tela.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+        ret_texto.center = (larguratela//2, alturatela//2)
+        tela.blit(textofinalmorte, ret_texto)
+        pygame.display.update()
 
     if len(lista_cobra) > comprimento_inicial:
         del lista_cobra[0]
